70821e3b3153cdb3f495ae44a29a766dee656755
GFSTee <georg.treitz@gmail.com>
2020-11-27 09:50:35 +0100

    react img

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import fotos from './data.json'
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="/food-photographer-jennifer-pallian-Z5tfVSjDeIk-unsplash.jpg" alt="" style={{ width: "200px" }} />
+      {/* hier in dem img tag beziehen wir uns auf den public Ordner  */}
+      <div className="background"></div>
+      {fotos.map(foto => <div key={foto.id} style={{ height: "50vh", background: `url(${foto.img})` }}>
+        {/* Auch hier beziehen wir uns auf den public Ordner! ACHTET DARAUF, DASS DIE DATEIEN KEINE LEERTASTEN HABEN*/}
+      </div>)}
+      {fotos.map(foto => <img key={foto.id} src={foto.img} style={{ width: "200px" }} />)}
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import './card.css'
+const Card = () => {
+    return (
+        <div className="my-card">
+            Card
+        </div>
+    );
+}
+
+export default Card;
